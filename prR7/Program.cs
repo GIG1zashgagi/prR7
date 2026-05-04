c3e75ff1e8d522995e2b03f54b4f3ca23e727c17
109234850921385 <iazavadskij15@gmail.com>
2026-05-04 10:38:51 +0300

    Класс, представляющий галактику

--- prR7/Program.cs
+++ prR7/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace GalaxiesDebugDemo
+{
+    public class Galaxy
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double DistanceLightYears { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Type}) - {DistanceLightYears} св. лет";
+        }
+    }
+}
